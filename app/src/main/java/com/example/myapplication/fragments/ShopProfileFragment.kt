b3e8049e982adcb27a88dc201c93405b94c0162f
Sauni Weerasinghe <it21373848@my.sliit.lk>
2023-05-10 21:04:03 +0530

    modified shop profile

--- app/src/main/java/com/example/myapplication/fragments/ShopProfileFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/ShopProfileFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.Login
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentProfileBinding
@@ -18,7 +19,11 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-
+class ShopViewModel  : ViewModel() {
+    var email: String? = null
+    var fname: String? = null
+    var lname: String? = null
+}
 class ShopProfileFragment : Fragment() {
     private var _binding: FragmentShopProfileBinding? = null
     private val binding get() = _binding!!
@@ -36,8 +41,38 @@ class ShopProfileFragment : Fragment() {
         return binding.root
     }
 
-    private fun checkUser() {
+    private val userViewModel by lazy {
+        ViewModelProvider(this).get(ShopViewModel::class.java)
+    }
 
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+
+        if (firebaseUser == null) {
+            startActivity(Intent(requireContext(), Login::class.java))
+            requireActivity().finish()
+        }else{
+            userViewModel.email = firebaseUser.email
+            binding.emailTv.text = userViewModel.email
+
+            ref.child(firebaseUser.uid)
+                .addListenerForSingleValueEvent(object: ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+
+                        userViewModel.fname= snapshot.child("fname").value as String?
+                        userViewModel.lname= snapshot.child("lname").value as String?
+                        binding.shopOwnerTv.text = "${userViewModel.fname} ${userViewModel.lname}"
+                    }
+                    override fun onCancelled(error: DatabaseError) {
+
+                    }
+
+
+                } )
+
+
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
