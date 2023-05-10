7251c62dd4caf8fdf8713b1f6a8e40c7aab45b7c
vvirajith <100179738+vvirajith@users.noreply.github.com>
2023-05-11 02:20:05 +0530

    modified

--- app/src/main/java/com/example/myapplication/AddReview.kt
+++ app/src/main/java/com/example/myapplication/AddReview.kt
@@ -1,22 +1,49 @@
 package com.example.myapplication
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityAddReviewBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class AddReview : AppCompatActivity() {
+    private lateinit var binding: ActivityAddReviewBinding
+
+    //firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    //progress dialog
+    private lateinit var progressDialog: ProgressDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_review)
+        binding = ActivityAddReviewBinding.inflate(layoutInflater)
 
+        setContentView(binding.root)
 
-        val submitReview = findViewById<Button>(R.id.submitReviewButton);
+        //init // firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
 
-        submitReview.setOnClickListener {
-            val nextPage = Intent(this, MainActivity::class.java);
-            startActivity(nextPage);
+        // configure progress dialog
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("please wait....")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        //handle click , go back
+        binding.addReviewBtn.setOnClickListener {
+            onBackPressed()
         }
+
+        //handle click,  begin upload category
+        binding.addReviewBtn
+            .setOnClickListener {
+            validateData()
+        }
+
 
         val cancelReview = findViewById<Button>(R.id.cancelReviewButton);
 
@@ -24,5 +51,51 @@ class AddReview : AppCompatActivity() {
             val nextPage = Intent(this, MainActivity::class.java);
             startActivity(nextPage);
         }
+    }
+    private var review = ""
+
+    private fun validateData() {
+        review = binding.addReviewBttn.text.toString().trim()
+
+        //validate data
+        if (review.isEmpty()) {
+            Toast.makeText(this, "Enter Category...", Toast.LENGTH_SHORT).show()
+        } else {
+            addReviewFirebase()
+
+        }
+    }
+
+    private fun addReviewFirebase() {
+        //show progress
+        progressDialog.show()
+
+        //get timestamp
+        val timestamp = System.currentTimeMillis()
+
+        // setting up data to add in firebase DB
+        val hashMap = HashMap<String, Any>()
+        hashMap["id"] = "$timestamp"
+        hashMap["review"]= review
+        hashMap["timestamp"] = timestamp
+        hashMap["uid"] = "${firebaseAuth.uid}"
+
+        //add to firebase DB: Database Root > categoryID  >category INfo
+        val ref = FirebaseDatabase.getInstance().getReference("review")
+        ref.child("$timestamp")
+            .setValue(hashMap)
+            .addOnCompleteListener {
+                // added successfully
+                progressDialog.dismiss()
+                Toast.makeText(this, "Added Successfully...", Toast.LENGTH_SHORT).show()
+
+
+            }
+            .addOnFailureListener {e->
+                //failed to add
+                progressDialog.dismiss()
+                Toast.makeText(this, "Failed to add due to ${e.message}", Toast.LENGTH_SHORT).show()
+
+            }
     }
 }
